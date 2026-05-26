892253a839f2d95d0e0bffb90fe599f35d140fa5
Nilltronic <38578463+nilltronic@users.noreply.github.com>
2026-05-26 06:14:38 +0100

    Add files via upload

--- rom_src/display_image_max_256_tiles.asm
+++ rom_src/display_image_max_256_tiles.asm
@@ -1,7 +1,6 @@
 ;; GameBoy code to display a single image.
-;; The image should use exactly 360 tiles and the tile map and tiles should be
-;; organized so that a tile addressing mode switch at LY == 90 will render the
-;; image correctly.
+;; The image should use at most 256 tiles since there's no mid-frame address
+;; mode switching being done to get more tiles.
 ;;
 ;; The tile map and tile data are left blank. The intention is to fill them in
 ;; after the ROM is already built by modifying the ROM file directly to plug in
@@ -11,7 +10,8 @@
 ;; The line with TileData.start gives the location for the tile data. The
 ;; locations are given as bank_number:offset. The bank_number is 00 for this
 ;; ROM since it doesn't use an MBC. The offset is in hexadecimal and tells you
-;; the byte index of where to start the data.SECTION "Header", ROM0[$100]
+;; the byte index of where to start the data.
+
 SECTION "Header", ROM0[$100]
 
 EntryPoint:
@@ -26,10 +26,9 @@ SECTION "Game code", ROM0
 
 Start:
 .waitVBlank
-  ld a, [$FF44]
+  ld a, [$FF40]
   cp 144 ; Check if the LCD is past VBlank
   jr c, .waitVBlank
-  ;; turn off screen by setting rLCDC to 0
   xor a ; equivalent to ld a, 0
   ld [$FF40], a
 
@@ -95,43 +94,15 @@ Start:
   ld a, %10010001
   ld [$FF40], a
 
-  ;; setup LYC to know when to switch addressing mode
-  ;; Using line 90 because it's after the first 128 tiles are rendered and
-  ;; before the the last 128 tiles. Any line that fits that criteria works.
-  ld a, 90
-  ld [$FF45], a
-
-  ;; setup interupts
-  ld a, %01000000
-  ld [$FF41], a ; turn on LY=LYC for LCD interupts
-
-  ld a, %00000010
-  ld [$FFFF], a
-
-.displayloop
-  ;; halt to wait for LYC interupt rather than use interupt handlers.
-  halt 
-  ;; switch addressing mode
-  ld a, %10000001
-  ld [$FF40], a
-  xor a
-  ld [$FF0F], a
-  ld a, %00000001
-  ld [$FFFF], a ; vblank interupts
+  ;; All the copying is done and LCDC is setup.
+  ;; Nothing to do now so just halt and let the gameboy display the image.
   halt
-  ;; switch addressing mode
-  ld a, %10010001
-  ld [$FF40], a
-  xor a
-  ld [$FF0F], a
-  ld a, %00000010
-  ld [$FFFF], a
-  jr .displayloop
-
-  
 
 SECTION "constants", ROM0
 TileByteCount:
+  ;; Uses 360 tiles because it was copied from another file that supports 360
+  ;; tiles. This should probably be changed to 256 since the code doesn't
+  ;; support more than that many tiles.
   DW 360 * 16
 
 SECTION "data", ROM0
@@ -146,5 +117,5 @@ TileData:
 .start
   ;; Fill blank tile data.
   ;; Filling enough space for 360 tiles, which is enough for 1 20x18 screen.
-  ;ds 360 * 16, $FF
+  ds 360 * 16, $FF
 .end
